0fbc6803aa4929ed850768568a68df7e69f7b91c
Carina Ogbat <carinawrightogbat@gmail.com>
2022-09-21 11:36:11 -0700

    saved name as a session

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -20,7 +20,7 @@ def show_homepage():
 @app.route('/form')
 def show_form():
     """Show form with message options."""
-    
+
     return render_template('form.html')
 
 @app.route('/results')
@@ -28,6 +28,23 @@ def show_results():
     """Show resulting message."""
 
     return render_template('results.html')
+
+@app.route('/save_name')
+def set_session():
+    """Set value for session['name']"""
+    # session['name'] = request.args.get("user_name")
+    # name = session['name']
+    name = request.args.get('user-name')
+    session['name'] = name
+
+    return render_template('homepage.html', name=name)
+
+# @app.route('/get-session')
+# def get_session():
+#     """Gets name value from session"""
+#     name = session['name']
+
+#     return render_template('homepage.html', name=name)
 
 
 if __name__ == "__main__":
